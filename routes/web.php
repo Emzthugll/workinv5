c839c7dd37e3651b46b289e9410ddb56e6a1b6eb
Emzthugll <marcjairus11@gmail.com>
2025-11-17 16:55:05 +0800

    added routes for policies page

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,36 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
+// Welcome page
 Route::get('/', function () {
     return Inertia::render('welcome', [
         'canRegister' => Features::enabled(Features::registration()),
     ]);
 })->name('home');
 
+
+
+// Dashboard (auth required)
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
 
+
+// Policies pages
+Route::get('/privacy-policy', function () {
+    return Inertia::render('policies/privacy'); 
+})->name('privacy-policy');
+
+Route::get('/how-it-works', function () {
+    return Inertia::render('policies/how'); 
+})->name('how-it-works');
+
+Route::get('/about-us', function () {
+    return Inertia::render('policies/about');
+})->name('about');
+
+
 require __DIR__.'/settings.php';
+
