91ec1f8170412cc79fa0fae0f40a864c6f969559
Emzthugll <marcjairus11@gmail.com>
2025-11-20 16:59:34 +0800

    roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,35 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Inertia::share([
+            'auth' => function () {
+                /** @var User|null $user */
+                $user = Auth::user();
+                
+                if ($user) {
+                    return [
+                        'user' => [
+                            'id' => $user->id,
+                            'name' => $user->name,
+                            'email' => $user->email,
+                            'roles' => $user->getRoleNames()->toArray(),
+                        ],
+                    ];
+                }
+            },
+        ]);
     }
 }
