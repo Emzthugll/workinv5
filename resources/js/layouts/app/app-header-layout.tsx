9c43a7098571d6a5d612258e1cd92463115051a6
Emzthugll <marcjairus11@gmail.com>
2025-11-20 17:03:09 +0800

    added navitem and right item

--- resources/js/layouts/app/app-header-layout.tsx
+++ resources/js/layouts/app/app-header-layout.tsx
@@ -1,17 +1,32 @@
 import { AppContent } from '@/components/app-content';
 import { AppHeader } from '@/components/app-header';
 import { AppShell } from '@/components/app-shell';
-import { type BreadcrumbItem } from '@/types';
-import type { PropsWithChildren } from 'react';
+import { type BreadcrumbItem, type NavItem } from '@/types';
+import { type PropsWithChildren } from 'react';
+import Footer from '@/components/footer';
+
+interface AppHeaderLayoutProps {
+    breadcrumbs?: BreadcrumbItem[];
+    navItems?: NavItem[];
+    rightItems?: NavItem[];
+    currentUrl?: string;
+}
 
 export default function AppHeaderLayout({
     children,
     breadcrumbs,
-}: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
+    navItems,
+    rightItems,
+}: PropsWithChildren<AppHeaderLayoutProps>) {
     return (
         <AppShell>
-            <AppHeader breadcrumbs={breadcrumbs} />
+            <AppHeader
+                breadcrumbs={breadcrumbs}
+                navItems={navItems} 
+                rightItems={rightItems} 
+            />
             <AppContent>{children}</AppContent>
+            <Footer/>
         </AppShell>
     );
 }
