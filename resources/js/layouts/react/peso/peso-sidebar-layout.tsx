a21dc3e4978e98f9a042872d5060472499af699b
Emzthugll <marcjairus11@gmail.com>
2025-11-24 14:56:06 +0800

    added peso sidebar layout

---
+++ resources/js/layouts/react/peso/peso-sidebar-layout.tsx
@@ -0,0 +1,21 @@
+import { AppContent } from '@/components/app-content';
+import { AppShell } from '@/components/app-shell';
+import { AppSidebarHeader } from '@/components/app-sidebar-header';
+import { PesoSidebar } from '@/components/react/peso/peso-sidebar';
+import { type BreadcrumbItem } from '@/types';
+import { type PropsWithChildren } from 'react';
+
+export default function PesoSidebarLayout({
+    children,
+    breadcrumbs = [],
+}: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
+    return (
+        <AppShell variant="sidebar">
+            <PesoSidebar/>
+            <AppContent variant="sidebar" className="overflow-x-hidden">
+                <AppSidebarHeader breadcrumbs={breadcrumbs} />
+                {children}
+            </AppContent>
+        </AppShell>
+    );
+}
