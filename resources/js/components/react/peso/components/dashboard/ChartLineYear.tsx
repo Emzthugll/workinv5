5848cca3a5d538951a1a7294ea2d56cdbcbf12c3
Emzthugll <marcjairus11@gmail.com>
2025-11-25 09:08:43 +0800

    added responsive container

--- resources/js/components/react/peso/components/dashboard/ChartLineYear.tsx
+++ resources/js/components/react/peso/components/dashboard/ChartLineYear.tsx
@@ -7,7 +7,15 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+    CartesianGrid,
+    Line,
+    LineChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from 'recharts';
 
 const yearData = [
     { year: '2021', Total: 250 },
@@ -26,25 +34,35 @@ export function ChartLineYear() {
                 </CardTitle>
                 <CardDescription>2021 - 2025</CardDescription>
             </CardHeader>
-            <CardContent>
-                <LineChart
-                    width={1160}
-                    height={400}
-                    data={yearData}
-                    margin={{ top: 30, right: 12, bottom: 20, left: 12 }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" vertical={false} />
-                    <XAxis dataKey="year" tickLine={false} axisLine={false} />
-                    <YAxis />
-                    <Tooltip />
-                    <Line
-                        type="monotone"
-                        dataKey="Total"
-                        stroke="#084896"
-                        strokeWidth={2}
-                        dot={true}
-                    />
-                </LineChart>
+
+            <CardContent className="h-[350px]">
+                <ResponsiveContainer width="100%" height="100%" debounce={200}>
+                    <LineChart
+                        data={yearData}
+                        margin={{
+                            top: 35,
+                            right: 12,
+                            bottom: 60,
+                            left: 12,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                        <XAxis
+                            dataKey="year"
+                            tickLine={false}
+                            axisLine={false}
+                        />
+                        <YAxis />
+                        <Tooltip />
+                        <Line
+                            type="monotone"
+                            dataKey="Total"
+                            stroke="#084896"
+                            strokeWidth={2}
+                            dot={true}
+                        />
+                    </LineChart>
+                </ResponsiveContainer>
             </CardContent>
         </Card>
     );
