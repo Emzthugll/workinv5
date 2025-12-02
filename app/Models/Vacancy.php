dcd031b9d23a5ecd8e7038b3f227dfa430ddc77e
Emzthugll <marcjairus11@gmail.com>
2025-12-02 11:33:14 +0800

    fixed the job application model

--- app/Models/Vacancy.php
+++ app/Models/Vacancy.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Log;
 
 class Vacancy extends Model
 {
@@ -14,35 +15,28 @@ class Vacancy extends Model
         'company_id',
         'title',
         'details',
-        'sub_specialization_id',
+        'sub_specialization_id', 
         'job_type',
-        'location',
-        'salary_from',
+        'location', 
+        'salary_from', 
         'salary_to',
-        'total_vacancy',
+        'total_vacancy'
     ];
 
-    /**
-     * The company that owns the vacancy.
-     */
+    
+
     public function company()
     {
         return $this->belongsTo(Company::class);
     }
 
-    /**
-     * The sub specialization for the vacancy.
-     */
     public function subSpecialization()
     {
         return $this->belongsTo(SubSpecialization::class);
     }
 
-    /**
-     * The job applications for this vacancy.
-     */
     public function jobApplications()
     {
-        return $this->hasMany(JobApplication::class); 
+        return $this->hasMany(JobApplication::class, 'vacancy_id', 'id');
     }
 }
